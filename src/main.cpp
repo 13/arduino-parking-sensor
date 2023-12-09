db2822d96c792740b79d902f87761b3b3c540395
13 <eggerben@gmail.com>
2023-12-09 23:43:30 +0100

    added support of esp

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,9 @@
 #include <LedController.hpp>
 #include <NewPing.h>
 #include "LedMatrixPatterns.h"
-
-#define VERBOSE
-// #define DEBUG
+#include "wsData.h"
+#include "helpers.h"
+#include "credentials.h"
 
 #if defined(ESP8266) || defined(ESP32)
 // MAX7218
@@ -40,15 +40,89 @@ unsigned int prevDistance = 0;
 
 boolean isCar = false;
 
+#if defined(ESP8266)
+String hostname = "esp8266-";
+#endif
+#if defined(ESP32)
+String hostname = "esp32-";
+#endif
+
+// WiFi & MQTT
+WiFiClient wifiClient;
+PubSubClient mqttClient(wifiClient);
+wsData myData;
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", 3600);
+
+long mqttLastReconnectAttempt = 0;
+int wsDataSize = 0;
+uint8_t connectedClients = 0;
+
+unsigned long previousMinute = 0;
+
+// supplementary functions
+#ifdef VERBOSE
+// one minute mark
+#define MARK
+unsigned long lastMillis = 0L;
+uint32_t countMsg = 0;
+#endif
+
 void setup()
 {
   Serial.begin(115200);
+  delay(10);
+#ifdef VERBOSE
+  delay(5000);
+#endif
+  // Start Boot
+  Serial.println(F("> "));
+  Serial.println(F("> "));
+  Serial.print(F("> Booting... Compiled: "));
+  Serial.println(VERSION);
+  Serial.print(F("> Node ID: "));
+  Serial.println(getUniqueID());
+  hostname += getUniqueID();
+#ifdef VERBOSE
+  Serial.print(("> Mode: "));
+  Serial.print(F("VERBOSE "));
+#ifdef DEBUG
+  Serial.print(F("DEBUG"));
+#endif
+  Serial.println();
+#endif
+  initFS();
+  checkWiFi();
+  mqttClient.setServer(mqtt_server, mqtt_port);
+#ifdef MQTT_SUBSCRIBE
+  mqttClient.setCallback(onMqttMessage);
+#endif
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    initMDNS();
+    connectToMqtt();
+    timeClient.begin();
+    timeClient.update();
+    myData.boottime = timeClient.getEpochTime();
+  }
+  // Initalize websocket
+  initWebSocket();
   lc.setIntensity(8); // Set the brightness (0 to 15)
   lc.clearMatrix();
 }
 
 void loop()
 {
+  ws.cleanupClients();
+#ifdef REQUIRES_INTERNET
+  checkWiFi();
+#endif
+  checkMqtt();
+#ifdef MARK
+  printMARK();
+#endif
   unsigned long currentMillis = millis();
 
   // Check if it's time to update the display
@@ -136,7 +210,7 @@ void loop()
       {
         counterTimeout++;
       }
-    }    
+    }
     else if (distance < 20 && !timeout)
     {
       if (state != 1)
